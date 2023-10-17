fe80e146bb26f274c38218158e6f91438da750d1
mmdiallo <2139175@cegepba.qc.ca>
2023-10-17 14:04:27 -0400

    Mise à jour du script de vBoxManage

--- scriptVBoxManage.ps1
+++ scriptVBoxManage.ps1
@@ -1,20 +1,23 @@
 param (
     [string]$ISOPath,
-    [string]$Storage,
+   # [string]$Storage,
     [string]$VMName,
-    [int]$VMRAM
+    [int]$VMRAM,
+    [string]$OSType
 )
+$env:PATH = $env:PATH + ";C:\Program Files\Oracle\VirtualBox"
 
 # Liste des noms de machines virtuelles
-$VMNames = @("${VMName}_SERVER2019", "${VMName}_WIN10")
 
-foreach ($VM in $VMNames) {
-    vboxmanage createvm --name $VM --ostype "Windows10_64" --register
 
-    vboxmanage storageattach $VM --storagectl "SATAController" --port 0 --device 0 --type dvddrive --medium $ISOPath
 
-    vboxmanage createhd --filename "$Storage\$VM.vdi" --size 20000
-    vboxmanage storageattach $VM --storagectl "SATAController" --port 1 --device 0 --type hdd --medium "$Storage\$VM.vdi"
+VBoxManage createvm --name $VMName --ostype $OSType --register
 
-    vboxmanage modifyvm $VM --memory $VMRAM --cpus 2
-}
+VBoxManage storagectl $VMName --name "SATA Controller" --add sata 
+
+VBoxManage storageattach $VMName --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium $ISOPath
+
+VBoxManage createhd --filename "$Storage\$VMName.vdi" --size 25000
+VBoxManage storageattach $VMName --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium "$Storage\$VMName.vdi"
+
+VBoxManage modifyvm $VMName --memory $VMRAM --cpus 2
